f058707ec6df8716b16c3288aaa77b23f2eb06f6
Brittany Welsh <hello@brittanywelsh.com>
2016-06-26 07:02:48 +0000

    Starter/sample code.

---
+++ challenges/codebusters/Player.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Send your busters out into the fog to trap ghosts and bring them home!
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int bustersPerPlayer = in.nextInt(); // the amount of busters you control
+        int ghostCount = in.nextInt(); // the amount of ghosts on the map
+        int myTeamId = in.nextInt(); // if this is 0, your base is on the top left of the map, if it is one, on the bottom right
+
+        // game loop
+        while (true) {
+            int entities = in.nextInt(); // the number of busters and ghosts visible to you
+            for (int i = 0; i < entities; i++) {
+                int entityId = in.nextInt(); // buster id or ghost id
+                int x = in.nextInt();
+                int y = in.nextInt(); // position of this buster / ghost
+                int entityType = in.nextInt(); // the team id if it is a buster, -1 if it is a ghost.
+                int state = in.nextInt(); // For busters: 0=idle, 1=carrying a ghost.
+                int value = in.nextInt(); // For busters: Ghost id being carried. For ghosts: number of busters attempting to trap this ghost.
+            }
+            for (int i = 0; i < bustersPerPlayer; i++) {
+
+                // Write an action using System.out.println()
+                // To debug: System.err.println("Debug messages...");
+
+                System.out.println("MOVE 8000 4500"); // MOVE x y | BUST id | RELEASE
+            }
+        }
+    }
+}
