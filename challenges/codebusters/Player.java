70e040c6c7e3abc3a3b9fb18b3bf590762c92bf3
Brittany Welsh <hello@brittanywelsh.com>
2016-07-01 19:53:20 -0600

    Set up basic OOP-oriented scaffolding

--- challenges/codebusters/Player.java
+++ challenges/codebusters/Player.java
@@ -1,133 +1,202 @@
-package codingame.challenges.codebusters;
+/*
+ * Comment out the package name line to sync file with Codingame:
+ */
+//package codingame.challenges.codebusters;
 
 import java.util.*;
 import java.io.*;
 import java.math.*;
 
+enum EntityType {
+    MY_UNIT,
+    ENEMY_UNIT,
+    GHOST
+}
+
+enum BasePosition {
+    TOP_LEFT(0, 0),
+    BOTTOM_RIGHT(16000, 9000);
+
+    private int _xPosition;
+    private int _yPosition;
+
+    BasePosition(int x, int y) {
+        _xPosition = x;
+        _yPosition = y;
+    }
+
+    int getX() {
+        return _xPosition;
+    }
+
+    int getY() {
+        return _yPosition;
+    }
+}
+
 /**
  * Send your busters out into the fog to trap ghosts and bring them home!
  **/
 class Player {
-
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int bustersPerPlayer = in.nextInt(); // the number of busters you control
-        int ghostCount = in.nextInt(); // the number of ghosts on the map
-        int myTeamId = in.nextInt(); // if this is 0, your base is on the top left of the map, if it is one, on the bottom right
+        int startNumberOfGhosts = in.nextInt(); // the number of ghosts on the map
+        int myTeamID = in.nextInt(); // if this is 0, your base is on the top left of the map; one, bottom right
 
-        System.err.println("ack");
+        BasePosition myBasePosition = getBasePositionFromTeamID(myTeamID);
+        debug("myBasePosition = " + myBasePosition);
+        debug("numberOfGhosts = " + startNumberOfGhosts);
 
-        // game loop
-        while (true) {
-            int numberOfVisibleEntities = in.nextInt(); // the number of busters and ghosts visible to you.
-      /* you can only "see" ghosts and rival busters when
-                                                           they are within 2200 units of one of your own busters.*/
-            Entity[] listOfVisibleEntities = new Entity[numberOfVisibleEntities];
+        while (true) { // Game loop
+            int numberOfVisibleEntities = in.nextInt(); // the number of busters and ghosts visible to you (within 2200 units of a buster)
+            List<Buster> listOfMyUnits = new ArrayList<>();
+            List<Entity> listOfVisibleGhosts = new ArrayList<>();
+            List<Entity> listOfVisibleEnemyUnits = new ArrayList<>();
             for (int i = 0; i < numberOfVisibleEntities; i++) {
-                int entityId = in.nextInt();
-                int x = in.nextInt();
-                int y = in.nextInt();
-                int entityType = in.nextInt();
-                int busterState = in.nextInt();
-                int behaviour = in.nextInt();
-                Entity newEntity = new Entity(entityId, // buster id or ghost id
-                                              x, // x-position of this buster / ghost
-                                              y, // y-position of this buster / ghost
-                                              entityType, // entity type: the team id if it is a buster, -1 if it is a ghost.
-                                              busterState, // state (busters only): 0=idle, 1=carrying a ghost.
-                                              behaviour, // For busters: Ghost id being carried. For ghosts: number of busters attempting to trap this ghost.
-                                              myTeamId);    // 0 or 1
-                System.err.println(newEntity.allAboutMe());
-                listOfVisibleEntities[i] = newEntity;
+                int entityID = in.nextInt(); // buster id or ghost id
+                int x = in.nextInt(); // x-position of this buster / ghost
+                int y = in.nextInt(); // y-position of this buster / ghost
+                int entityType = in.nextInt(); // entity type: the team id if it is a buster, -1 if it is a ghost.
+                int busterState = in.nextInt(); // state (busters only): 0=idle, 1=carrying a ghost.
+                int numberOfInteractions = in.nextInt(); // For busters: Ghost id being carried. For ghosts: number of busters attempting to trap this ghost.
+
+                EntityType thisEntityType = getEntityTypeFromInt(entityType, myTeamID);
+                switch (thisEntityType) {
+                    case MY_UNIT:
+                        listOfMyUnits.add(new Buster(entityID, x, y, busterState, numberOfInteractions));
+                        break;
+                    case ENEMY_UNIT:
+                        listOfVisibleEnemyUnits.add(new Buster(entityID, x, y, busterState, numberOfInteractions));
+                        break;
+                    case GHOST:
+                        listOfVisibleGhosts.add(new Ghost(entityID, x, y, numberOfInteractions));
+                        break;
+                    default:
+                        break;
+                }
             }
 
             for (int i = 0; i < bustersPerPlayer; i++) {
+                Buster thisBuster = listOfMyUnits.get(i);
+
+                // To debug: System.err.println("Debug messages...");
+                debug("i=" + i + "  " + thisBuster.aboutMe());
 
                 // Write an action using System.out.println()
-                // To debug: System.err.println("Debug messages...");
-
                 System.out.println("MOVE 8000 4500"); // MOVE x y | BUST id | RELEASE
-                //System.out.println("BUST ");
             }
         }
     }
 
-    private int getNearestGhost() {
-        return 0;
+    private static void debug(String debugMessage) {
+        System.err.println(debugMessage);
     }
 
-
-    private static class Entity {
-        private int _entityId; // buster id or ghost id
-        private int _xPosition; // x=0 left to x=*** bottom
-        private int _yPosition; // y=0 top to y=*** bottom
-        private int _entityType; // the team id if it is a buster, -1 if it is a ghost.
-        private int _state; // For busters: 0=idle, 1=carrying a ghost.
-        private int _value; // For busters: Ghost id being carried. For ghosts: number of busters attempting to trap this ghost.
-        private int _myTeamId; // 0 for me on top left base; 1 for bottom right base
-
-        private Entity() {
-        }
-
-        public Entity(int entityId,
-                      int xPosition,
-                      int yPosition,
-                      int entityType,
-                      int state,
-                      int value,
-                      int myTeamId) {
-            _entityId = entityId;
-            _xPosition = xPosition;
-            _yPosition = yPosition;
-            _entityType = entityType;
-            _state = state;
-            _value = value;
-            _myTeamId = myTeamId;
-        }
-
-        public int getEntityId() {
-            return _entityId;
-        }
-
-        public int getX() {
-            return _xPosition;
-        }
-
-        public int getY() {
-            return _yPosition;
-        }
-
-        public EntityType getEntityType() {
-            if (_entityType == -1) {
-                return EntityType.GHOST;
-            }
-            else if (_entityType == _myTeamId) {
-                return EntityType.MY_UNIT;
-            }
-            else {
-                return EntityType.RIVAL_UNIT;
-            }
-        }
-
-        public String allAboutMe() {
-            return "Entity ID = " + getEntityType().toString() + "_" + getEntityId() + "\n--X: " + this.getX() + ",  Y: " + this.getY();
+    private static EntityType getEntityTypeFromInt(int entityType,
+                                                   int myTeamID) {
+        if (entityType == -1) {
+            return EntityType.GHOST;
+        } else if (entityType == myTeamID) {
+            return EntityType.MY_UNIT;
+        } else {
+            return EntityType.ENEMY_UNIT;
         }
     }
 
-    public enum EntityType {
-        MY_UNIT,
-        RIVAL_UNIT,
-        GHOST
+    private static BasePosition getBasePositionFromTeamID(int teamID) {
+        if (teamID == 0) {
+            return BasePosition.TOP_LEFT;
+        } else {
+            return BasePosition.BOTTOM_RIGHT;
+        }
+    }
+}
+
+class Entity {
+    int _entityID; // starts from 0
+    int _xPosition; // x=0 left
+    int _yPosition; // y=0 top
+    EntityType _entityType;
+
+    Entity() {
     }
 
-    public enum BusterState {
-        IDLE,
-        CARRYING_UNIT
+    private int getID() {
+        return _entityID;
     }
 
-    public enum MyBasePosition {
-        TOP_LEFT,
-        BOTTOM_RIGHT
+    private int getX() {
+        return _xPosition;
     }
 
+    private int getY() {
+        return _yPosition;
+    }
+
+    private EntityType getEntityType() {
+        return _entityType;
+    }
+
+    String aboutMe() {
+        return "Entity:  " + getEntityType().toString() + "_" + getID() + "\n    X: " + this.getX() + ",  Y: " + this.getY();
+    }
+}
+
+class Buster extends Entity {
+    private boolean _isCarryingAGhost;
+    private int _idOfGhostBeingCarried;
+
+    private Buster() {
+    }
+
+    Buster(int entityID,
+           int xPosition,
+           int yPosition,
+           int busterState,
+           int numberOfInteractions) {
+        _entityType = EntityType.MY_UNIT;
+        _entityID = entityID;
+        _xPosition = xPosition;
+        _yPosition = yPosition;
+
+        if (busterState == 0 && numberOfInteractions == -1) {
+            _isCarryingAGhost = false;
+        } else if (busterState == 1 & numberOfInteractions > -1) {
+            _isCarryingAGhost = true;
+            _idOfGhostBeingCarried = numberOfInteractions;
+        } else {
+            throw new IllegalArgumentException("Invalid buster state!");
+        }
+    }
+
+    boolean isCarryingAGhost() {
+        return _isCarryingAGhost;
+    }
+
+    int idOfGhostBeingCarried() {
+        return _idOfGhostBeingCarried;
+    }
+}
+
+class Ghost extends Entity {
+    private int _numberOfBusterThreats;
+
+    private Ghost() {
+    }
+
+    Ghost(int entityID,
+          int xPosition,
+          int yPosition,
+          int numberOfInteractions) {
+        _entityType = EntityType.GHOST;
+        _entityID = entityID;
+        _xPosition = xPosition;
+        _yPosition = yPosition;
+        _numberOfBusterThreats = numberOfInteractions;
+    }
+
+    int getNumberOfBusterThreats() {
+        return _numberOfBusterThreats;
+    }
 }
