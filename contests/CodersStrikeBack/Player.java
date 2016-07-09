f57e20a6eb600692e333dddc88ca00944e7e65f5
Brittany Welsh <hello@brittanywelsh.com>
2016-07-08 21:31:49 -0600

    Coders Strike Back - starting point wood 1 league

---
+++ contests/CodersStrikeBack/Player.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop
+        while (true) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            int nextCheckpointX = in.nextInt(); // x position of the next check point
+            int nextCheckpointY = in.nextInt(); // y position of the next check point
+            int nextCheckpointDist = in.nextInt(); // distance to the next checkpoint
+            int nextCheckpointAngle = in.nextInt(); // angle between your pod orientation and the direction of the next checkpoint
+            int opponentX = in.nextInt();
+            int opponentY = in.nextInt();
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            int speed;
+            if (nextCheckpointAngle > 90 || nextCheckpointAngle < -90) {
+                speed =0;
+            } else {
+                speed = 100;
+            }
+            
+            System.out.println(nextCheckpointX + " " + nextCheckpointY + " " + speed);
+        }
+    }
+}
